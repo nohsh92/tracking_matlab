3981396acf5bb08289b4de6a14e3f3787a619646
nohsh92 <nohsh92@gmail.com>
2021-05-14 00:38:52 +0900

    Update tracker.m
    added target

--- tracker.m
+++ tracker.m
@@ -1,17 +1,23 @@
 clear; clc;
 
 dt = 1;
-iterations = 5;
+iterations = 6;
 transition = [1 0 dt 0; 0 1 0 dt; 0 0 1 0; 0 0 0 1];
-x = zeros(4, iterations);
+target = zeros(4, iterations);
+ownship = zeros(4, iterations);
 
-x(:,1) = [1 5 0 -1]';
+target(:,1) = [1 5 0 -1]';
+ownship(:,1) = [0 0 0 1]';
 
 for i = 2:iterations
-   x(:,i) = transition * x(:,i-1);
+   target(:,i) = transition * target(:,i-1);
+   ownship(:,i) = transition * ownship(:,i-1);
 end
 
-plot_x = x(1,1:5);
-plot_y = x(2,1:5);
+plot_target_x = target(1, 1:5);
+plot_target_y = target(2, 1:5);
 
-plot(plot_x,plot_y)
+plot_ownship_x = ownship(1, 1:5);
+plot_ownship_y = ownship(2, 1:5);
+
+plot(plot_target_x,plot_target_y, plot_ownship_x, plot_ownship_y)
